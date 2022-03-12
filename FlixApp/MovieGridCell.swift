5b3400c6bb2744afdface2e9de3dd9ae1016f5a2
ugwe <michaelugwe01@gmail.com>
2022-03-11 20:38:35 -0500

    part 2 completed
    Finished part 2 of the flix app

---
+++ FlixApp/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  FlixApp
+//
+//  Created by Michael Ugwe on 3/11/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
